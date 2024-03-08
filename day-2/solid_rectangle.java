ce4759cd277417a8465121bc6e35473ac61cb615
Jatin <chaurasiyajatin68@gmail.com>
2024-03-08 22:25:08 +0530

    day-1

---
+++ day-2/solid_rectangle.java
@@ -0,0 +1,16 @@
+
+public  class solid_rectangle
+{
+    public static void main(String[] args)
+    {
+        for(int i = 0;i<4;i++){
+            for(int j = 1;j<=5;j++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        // for(int i = 0;i <4;i++){
+        //     System.out.println("* * * * *");
+        // }
+    }
+}
