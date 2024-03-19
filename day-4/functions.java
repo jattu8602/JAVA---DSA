e0c4c2366b00e0d73c3eed49c275872a20134f41
Jatin <chaurasiyajatin68@gmail.com>
2024-03-19 22:42:15 +0530

    jatin

---
+++ day-4/functions.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public  class functions
+{
+    public static void printMyName(String name){
+        System.out.println(name);
+
+        return;
+
+    }
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String name  = sc.next();
+
+        printMyName(name);//function call
+
+    }
+}
