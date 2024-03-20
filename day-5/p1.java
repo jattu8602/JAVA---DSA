d481849fd2142e4f1503804aaa6d598395cca6aa
Jatin <chaurasiyajatin68@gmail.com>
2024-03-20 22:54:27 +0530

    jatin

--- day-5/p1.java
+++ day-5/p1.java
@@ -1,5 +1,19 @@
-package day-5;
+import java.util.*;
 
-public class p1 {
-    
+public  class p1
+{
+    public static int  average(int a ,int b, int c){
+        return  (a+b+c)/3;
+    }
+    public static void main(String[] args)
+    {
+        Scanner  sc = new Scanner(System.in);
+        System.out.println("Enter the three numbers : ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println("average is :");
+        System.out.print(average(a,b,c));
+    }
+
 }
