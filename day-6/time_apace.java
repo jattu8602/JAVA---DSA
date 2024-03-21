4934d31a500ffa933f5f9ea9d31876e9b6ea6b10
Jatin <chaurasiyajatin68@gmail.com>
2024-03-21 21:08:05 +0530

    ghf

---
+++ day-6/time_apace.java
@@ -0,0 +1,28 @@
+/*
+Time complexicity
+Relation between Input size  and Running Time (Operatons)
+
+input = time complexicity
+
+There are three types of time complexicity
+ Best case                   - omega(1)
+ Average case                -theta((n+1)/2)
+ Worst Case                  - O(n^2) or Omega(n!)
+
+
+
+
+
+
+
+
+
+
+
+
+
+ Space Complexity
+ 
+
+
+ */
